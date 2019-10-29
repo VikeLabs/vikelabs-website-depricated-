d33ff35d04bf89aecbce15c9d8883ab85d98db79
Michelle Song <michellesong@w134-87-146-135.wireless.uvic.ca>
2019-10-28 18:47:25 -0700

    my first commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from '../../assets/images/logo.png';
+import './Header.css';
 
 
 function Header() {
   return (
-    <header>
+    <header id="header">
       <img src={logo} className="App-logo" alt="logo" />
+      <div id="logo-bg"> </div>
     </header>
   );
 }
