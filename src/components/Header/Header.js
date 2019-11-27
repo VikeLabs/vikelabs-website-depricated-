6f221a6b6aac90de9abea6d1732c61e3c0c8ab43
Jordan Benning <jbenning97@gmail.com>
2019-11-27 14:55:03 -0800

    logo routes to home page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import logo from '../../assets/images/logo.png';
-
+import React from "react";
+import logo from "../../assets/images/logo.png";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className='app-header'>
-      <img src={logo} className="app-logo" alt="logo" />
+    <header className="app-header">
+      <Link to='/'>
+        <img src={logo} className="app-logo" alt="logo" />
+      </Link>
     </header>
   );
 }
